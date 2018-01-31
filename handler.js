4d9858096e97d460c7c32f530e27ea534f4a6724
prudhvirajmudunuri <pm6y6@mail.umkc.edu>
2018-01-30 21:41:13 -0500

    initial commit

--- handler.js
+++ handler.js
@@ -1,16 +1,18 @@
 
 import { helloSvc } from './services/helloSvc';
+import TacoGallery from './services/TacoGallery';
 
-export const helloLambda = (event, context, callback) => {
+const isString = obj => typeof obj === "string" || obj instanceof String;
 
-  const result = helloSvc({name: 'Serverless'});
+module.exports.helloLambda = (event, context, callback) => {
+  const result = helloSvc({ name: "Serverless" });
 
   const response = {
     statusCode: 200,
     body: JSON.stringify({
       message: result,
-      input: event,
-    }),
+      input: event
+    })
   };
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
@@ -19,7 +21,4 @@ export const helloLambda = (event, context, callback) => {
   callback(null, response);
 };
 
-export default {
-  helloLambda
-}
 
